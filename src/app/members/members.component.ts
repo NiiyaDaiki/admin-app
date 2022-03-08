93e42c00f1cbe27117b02ff1c4eab1364ebfb05c
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2022-03-09 01:31:01 +0900

    memberをinterface化

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Member } from '../member';
+
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css'],
 })
 export class MembersComponent implements OnInit {
-  member = '田中太郎';
+  member: Member = {
+    id: 1,
+    name: '田中太郎',
+  };
 
   constructor() {}
 
